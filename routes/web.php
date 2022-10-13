db0ad29fb22798474d8b8cb48f61eac1213826a4
j20101co <c94585004@gmail.com>
2022-10-13 09:34:32 +0900

    third commit

--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,10 @@ Route::get('/', function () {
 });
  */
 
-Route::get('/posts',[PostController::class, 'index']);
+Route::get('/',[PostController::class, 'index']);
+
+/*
+Route::get('/', function() {
+	 return view('posts/index');
+});
+ */
