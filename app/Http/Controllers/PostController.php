df4e570418b49afc6373b1f59260e35c2ed549f3
j20101co <c94585004@gmail.com>
2022-10-22 06:54:12 +0900

    fifth commit

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+//use Illuminate\Http\Request;
 use App\Models\Post;
+use App\Http\Requests\PostRequest;
 
 class PostController extends Controller
 {
@@ -15,6 +16,22 @@ class PostController extends Controller
 	}
 	public function create(){
 		return view('posts/create');
+	}
+
+	public function store(PostRequest $request, Post $post){
+		$input = $request['post'];
+		$post->fill($input)->save();
+		return redirect('/posts/' . $post->id);
+	}
+
+	public function edit(Post $post){
+		return view('posts/edit')->with(['post' => $post]);
+	}
+
+	public function update(PostRequest $request, Post $post){
+		$input_post = $request['post'];
+		$post->fill($input_post)->save();
+		return redirect('/posts/' . $post->id);
 	}
 }
  
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,20 +2,22 @@
 <html lang="{{ str_replace("_","-",app()->getLocale()) }}">
   <head>
     <meta charset="utf-8">
-    <title>Blog</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Posts</title>
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stlyesheet">
   </head>
-  <body class ="antialiased">
+  <body>
     <h1 class="title">
       {{ $post->title }}
     </h1>
     <div class="content">
       <div class="content__post">
         <h3>本文</h3>
-        <p class='body'>{{ $post->body }}</p>
+        <p>{{ $post->body }}</p>
       </div>
     </div>
+    <p class="edit">[<a href="/posts/{{ $post->id }}/edit">edit</a>]</p>
     <div class="footer">
       <a href="/">戻る</a>
     </div>
